115c30283233528d2fde62077e8bcb36ce7a9e06
ahmed9949 <aosma9949@gamil.com>
2024-02-21 17:40:34 +0200

    MVC

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    Array.from(document.querySelectorAll('.card input')).forEach((input, i) => {
-        input.addEventListener('keyup', () => {
-            const caret = input.closest('.card').querySelector('.bi-caret-down-fill');
-            caret.style.transform = input.value.length > 0 ? 'rotate(180deg)' : 'rotate(0deg)';
-        });
-    });
-});
